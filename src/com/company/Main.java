a644be74204b3d3b5fe2d8bcd3303f8aadf4e479
DonConteo <dmitriy.tsoy.russia@gmail.com>
2020-08-22 22:40:41 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.Collections;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(String.join("", Collections.nCopies(1000, "Ты должен мне пиво\n")));
+    }
+}
